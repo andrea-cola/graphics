b08e23e3e2b81e42dd60974ee0a3625d3e949973
andrea-cola <andrea.cola95@gmail.com>
2019-07-06 17:22:46 +0200

    Two direction rotation.

--- rotate.js
+++ rotate.js
@@ -9,28 +9,42 @@ function rotate(){
     var lat = 40.718655;
     var long = 74.004589;
 
-    console.log(flag, angle, elevation);
-
-    var points = [];
+    // generate interpolation points for long.
+    var pLong = [];
     for(var i = 0; i < 4; i++)
-        points[i] = currentLong + i * (long - currentLong) / 3;
+        pLong[i] = currentLong + i * (long - currentLong) / 3;
 
-    bezierLoop(0, points)
+    // generate interpolation points for lat.
+    var pLat = [];
+    for(var i = 0; i < 4; i++)
+        pLat[i] = currentLat + i * (lat - currentLat) / 3;
+
+    bezierLoop(0, pLat, pLong)
 }
 
-function bezierLoop(alpha, points){
-    angle = bezier(alpha, points);
-    console.log(alpha, angle);
+/**
+ * Animated rotation of the earth. 
+ */
+function bezierLoop(alpha, pLat, pLong){
+    angle = bezier(alpha, pLong);
+    elevation = bezier(alpha, pLat);
+    
     if(alpha <= 1)
         setTimeout(function() {
-            bezierLoop(alpha + 0.01, points)
+            bezierLoop(alpha + 0.01, pLat, pLong)
         }, 10);
 }
 
+ /**
+  * Bezier curve interpolation.
+  */
 function bezier(a, p){
     return Math.pow(1-a, 3) * p[0] + 3 * Math.pow(1-a, 2) * a * p[1] + 3 * (1-a) * Math.pow(a, 2) * p[2] + Math.pow(a, 3) * p[3]; 
 }
 
+/**
+ * Reset the view to the primar meridian.
+ */
 function reset(){
     angle = 0.01;
     elevation = 0.01;
